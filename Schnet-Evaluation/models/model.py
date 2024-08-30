5962492680944464be9802d1f659df996d17035d
littlemoonstones <littlemoonstones@gmail.com>
2024-08-30 20:22:56 +0800

    first commit

---
+++ Schnet-Evaluation/models/model.py
@@ -0,0 +1,44 @@
+import ptan
+import numpy as np
+from lib.ModelType import TrainingData
+from typing import List
+
+class AgentDDPG(ptan.agent.BaseAgent):
+    """
+    Agent implementing Orstein-Uhlenbeck exploration process
+    """
+    def __init__(self, net, device="cpu", ou_enabled=True,
+                 ou_mu=0.0, ou_teta=0.15, ou_sigma=0.2,
+                 ou_epsilon=1.0):
+        self.net = net
+        self.device = device
+        self.ou_enabled = ou_enabled
+        self.ou_mu = ou_mu
+        self.ou_teta = ou_teta
+        self.ou_sigma = ou_sigma
+        self.ou_epsilon = ou_epsilon
+
+    def initial_state(self):
+        return None
+
+    def __call__(self, states: List[TrainingData], agent_states):
+        mu_v = self.net(states)
+        actions = mu_v.data.cpu().numpy()
+
+        if self.ou_enabled and self.ou_epsilon > 0:
+            new_a_states = []
+            for a_state, action in zip(agent_states, actions):
+                if a_state is None:
+                    a_state = np.zeros(
+                        shape=action.shape, dtype=np.float32)
+                a_state += self.ou_teta * (self.ou_mu - a_state)
+                a_state += self.ou_sigma * np.random.normal(
+                    size=action.shape)
+
+                action += self.ou_epsilon * a_state
+                new_a_states.append(a_state)
+        else:
+            new_a_states = agent_states
+
+        actions = np.clip(actions, -1, 1)
+        return actions, new_a_states
